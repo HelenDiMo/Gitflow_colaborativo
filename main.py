a264059ae300814baea53e5333a24bdafce8d6f6
HelenDiMo <elena.diazmoreno@gmail.com>
2026-04-21 13:20:32 +0200

    fix/: adding comments

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+# Coder A - suma de 2 variables
 a = 2
 b = 3
 def add(x, y):
     return x + y
 result = add(a, b)
-print("The result of adding", a, "and", b, "is:", result)
+print("The result of adding", a, "and", b, "is:", result) # EN
