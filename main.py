d677508b3d6a61d7b3a28012cc8482914ff7dcda
adrianaarang <129066063+adrianaarang@users.noreply.github.com>
2026-04-21 13:29:25 +0200

    feat: resta y crear conflicto

--- main.py
+++ main.py
@@ -1,7 +1,21 @@
-# Coder A - suma de 2 variables
+# Coder A - suma de 2 variables. crear conflicto
 a = 2
 b = 3
 def add(x, y):
     return x + y
 result = add(a, b)
 print("The result of adding", a, "and", b, "is:", result) # EN
+
+
+
+
+# Coder B - resta de 2 variables
+a = 2
+b = 3
+def subtract(x, y):
+    return x - y
+result = subtract(a, b)
+print("The result of subtracting", a, "and", b, "is:", result) # EN
+
+
+
